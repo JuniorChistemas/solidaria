9213f5468698634fd99551e69804ababdc35d11d
juniorChistemas <junior15062023@gmail.com>
2025-03-21 07:21:53 -0500

    add changes for docker

--- routes/web.php
+++ routes/web.php
@@ -54,7 +54,6 @@ Route::middleware(['auth:sanctum', config('jetstream.auth_session'), 'verified',
 Route::get('doctors/search', [DoctorController::class, 'searchDoctor'])->name('doctor.search');
 
 Route::get('laboratories/search', [LaboratoryController::class, 'searchLaboratory'])->name('laboratory.search');
-<<<<<<< HEAD
 Route::get('laboratory/list', [LaboratoryController::class, 'listlaboratory'])->name('laboratory.list');
 Route::post('laboratory/add/', [LaboratoryController::class, 'store'])->name('laboratory.store');
 Route::put('laboratory/update/{laboratory}', [LaboratoryController::class, 'update'])->name('laboratory.update');
@@ -62,12 +61,3 @@ Route::delete('laboratory/delete/{laboratory}', [LaboratoryController::class, 'd
 
 Route::get('zones/search', [ZoneController::class, 'searchZone'])->name('zone.search');
 Route::get('categories/search', [ZoneController::class, 'searchCategory'])->name('category.search');
-=======
-
-Route::get('zones/search', [ZoneController::class, 'searchZone'])->name('zone.search');
-Route::get('categories/search', [ZoneController::class, 'searchCategory'])->name('category.search');
-Route::get('category/list', [CategoryController::class, 'listCategory'])->name('category.list');
-Route::post('category/add/', [CategoryController::class, 'store'])->name('category.store');
-Route::put('category/update/{category}', [CategoryController::class, 'update'])->name('category.update');
-Route::delete('category/delete/{category}', [CategoryController::class, 'destroy'])->name('category.destroy');
->>>>>>> ff5d16b2ffab6b50236bfce8f46669f5bcb9ca6b
